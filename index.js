29d34ce83c35a72d1c52e55f186f023aabda185d
domm <dommferger@gmail.com>
2023-08-22 11:27:54 -0400

    need lots of practice

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(){
+   const newDrivers = drivers.slice(0,2);
+   // console.log(newDrivers);
+    return newDrivers
+}
+returnFirstTwoDrivers();
+
+function returnLastTwoDrivers(){
+        const newDrivers = drivers.slice(2,4);
+       // console.log(newDrivers);
+        return newDrivers;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+//The important thing to note is that createFareMultiplier() is not directly multiplying the fare.
+// It is creating a new function that can be used to multiply fares by a specific factor.
+// This allows for more flexibility and reusability in the code.
+// Yes I am aware I can make this a block.
+
+function createFareMultiplier(integer){
+    return function fare(price){
+        return price * integer
+    }
+}
+//createFareMultiplier(4);
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(driversArray, driversFunction){
+    if(driversFunction == returnFirstTwoDrivers){
+        return driversArray.slice(0,2);
+    } else if (driversFunction == returnLastTwoDrivers){
+        return driversArray.slice(driversArray.length - 2);
+    }
+}
